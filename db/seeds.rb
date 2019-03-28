085d1a1e863a828c2870696f2242802d63055975
yFGq-Ziw <rui_aso@live.jp>
2019-03-28 04:47:17 +0000

    for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-Task.create(status: 'test1', content: 'test content 1')
-Task.create(status: 'test2', content: 'test content 2')
-Task.create(status: 'test3', content: 'test content 3')
-
 (1..100).each do |number|
   Task.create(status: 'test ' + number.to_s, content: 'test content ' + number.to_s)
 end
