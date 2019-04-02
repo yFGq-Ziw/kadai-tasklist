a7675c46b318381fce2a087a69bfe0cd9073b213
yFGq-Ziw <rui_aso@live.jp>
2019-04-02 08:39:40 +0000

    shusei2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy, :update]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.order(created_at: :desc).page(params[:page]).per(10)
